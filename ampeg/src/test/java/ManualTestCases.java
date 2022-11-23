10780a408287498ac1741aaa181c052a6358f193
MitchellMaliglig <mitchell.maliglig@gmail.com>
2022-11-22 16:38:18 -0800

    added test case stubs

---
+++ ampeg/src/test/java/ManualTestCases.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class ManualTestCases extends AmpegTests{
+	@Test
+	public void navigateToFaqPage() {
+		
+	}
+	
+	@Test
+	public void navigateToSvt810eThroughJustinPearson() {
+		
+	}
+	
+	@Test
+	public void navigateToProductManualsPage() {
+		
+	}
+}
