edf9e68faee2f44c170c4836ae94e66311139bda
MitchellMaliglig <mitchell.maliglig@gmail.com>
2022-12-12 23:08:04 -0800

    created class Bass

---
+++ java.io/src/test/java/Bass.java
@@ -0,0 +1,37 @@
+
+public class Bass {
+	private String make;
+	private String model;
+	private int stringCount;
+
+	public Bass(String make, String model, int stringCount) {
+		this.make = make;
+		this.model = model;
+		this.stringCount = stringCount;
+	}
+	
+	public String getMake() {
+		return this.make;
+	}
+	
+	public void setMake(String make) {
+		this.make = make;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public int getStringCount() {
+		return this.stringCount;
+	}
+	
+	public void setStringCount(int stringCount) {
+		this.stringCount = stringCount;
+	}
+
+}
