d8ac3151c5643e8817e850711f94584afe7557c6
MitchellMaliglig <mitchell.maliglig@gmail.com>
2022-12-13 01:43:31 -0800

    added equals()

--- java.io/src/test/java/Bass.java
+++ java.io/src/test/java/Bass.java
@@ -9,34 +9,51 @@ public class Bass {
 		this.model = model;
 		this.stringCount = stringCount;
 	}
-	
+
 	public String getMake() {
 		return this.make;
 	}
-	
+
 	public void setMake(String make) {
 		this.make = make;
 	}
-	
+
 	public String getModel() {
 		return this.model;
 	}
-	
+
 	public void setModel(String model) {
 		this.model = model;
 	}
-	
+
 	public int getStringCount() {
 		return this.stringCount;
 	}
-	
+
 	public void setStringCount(int stringCount) {
 		this.stringCount = stringCount;
 	}
-	
+
 	@Override
 	public String toString() {
 		return getMake() + " " + getModel() + " " + getStringCount();
 	}
 
+	@Override
+	public boolean equals(Object object) {
+		if (object == this) {
+			return true;
+		}
+
+
+		if (!(object instanceof Bass)) {
+			return false;
+		}
+
+		Bass bass = (Bass) object;
+
+		return this.getMake() == bass.getMake() && 
+				this.getModel() == bass.getModel() &&
+				this.getStringCount() == bass.getStringCount();
+	}
 }
