b32fe7dc94051dde8a44440f118c781db8bc4e2e
MitchellMaliglig <mitchell.maliglig@gmail.com>
2023-01-09 21:16:00 -0800

    moved stuff

---
+++ ampeg/src/test/java/AmpegSteps.java
@@ -0,0 +1,11 @@
+
+import io.cucumber.java.en.Given;
+
+public class AmpegSteps extends AmpegTests{
+
+	@Given("^I am on the home page$")
+    public void userOnHomePage() throws Throwable {
+        
+    }
+
+}
