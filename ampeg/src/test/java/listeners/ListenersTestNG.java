7677c09998ea36865684091d36008704edb4b454
MitchellMaliglig <mitchell.maliglig@gmail.com>
2023-01-20 20:37:26 -0800

    refactored generateReport()

--- ampeg/src/test/java/listeners/ListenersTestNG.java
+++ ampeg/src/test/java/listeners/ListenersTestNG.java
@@ -1,14 +1,15 @@
 package listeners;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import org.testng.IReporter;
 import org.testng.ISuite;
-import org.testng.ISuiteResult;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.internal.collections.Pair;
 import org.testng.xml.XmlSuite;
 
 public class ListenersTestNG implements ITestListener, IReporter {
@@ -43,31 +44,24 @@ public class ListenersTestNG implements ITestListener, IReporter {
 	@Override
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
 
-		for (ISuite suite : suites) {
-			String suiteName = suite.getName();
-			Map<String, ISuiteResult> suiteResults = suite.getResults();
-			
-			for (ISuiteResult sr : suiteResults.values()) {
-				ITestContext tc = sr.getTestContext();
-				var passed = tc.getPassedTests().getAllResults();
-				var failed = tc.getFailedTests().getAllResults();
-				var skipped = tc.getSkippedTests().getAllResults();
-				
-				System.out.println("Passed tests for suite '" + suiteName + "' is:" + passed.size());
-				for (var test : passed) {
-					System.out.println(test.getName());
-				}
-				System.out.println();
-				
-				System.out.println("Failed tests for suite '" + suiteName + "' is:" + failed.size());
-				for (var test : failed) {
-					System.out.println(test.getName());
-				}
-				System.out.println();
-				
-				System.out.println("Skipped tests for suite '" + suiteName + "' is:" + skipped.size());
-				for (var test : skipped) {
-					System.out.println(test.getName());
+		for (var suite : suites) {
+			var suiteName = suite.getName();
+			var suiteResults = suite.getResults();
+
+			for (var suiteResult : suiteResults.values()) {
+				var testContext = suiteResult.getTestContext();
+
+				var list = new ArrayList<Pair<String, Set<ITestResult>>>();
+				list.add(new Pair<String, Set<ITestResult>>("passed", testContext.getPassedTests().getAllResults()));
+				list.add(new Pair<String, Set<ITestResult>>("failed", testContext.getFailedTests().getAllResults()));
+				list.add(new Pair<String, Set<ITestResult>>("skipped", testContext.getSkippedTests().getAllResults()));
+
+				for (var pair : list) {
+					System.out.println(pair.first() + " tests for suite '" + suiteName + "' is:" + pair.second().size());
+					for (var test : pair.second()) {
+						System.out.println(test.getName());
+					}
+					System.out.println();
 				}
 			}
 		}
