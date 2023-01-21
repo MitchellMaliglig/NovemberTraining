55b303a9f8f12b9b6575eb9215590ba2295d6d41
MitchellMaliglig <mitchell.maliglig@gmail.com>
2023-01-20 18:20:03 -0800

    added generateReport()

--- ampeg/src/test/java/listeners/ListenersTestNG.java
+++ ampeg/src/test/java/listeners/ListenersTestNG.java
@@ -1,10 +1,17 @@
 package listeners;
 
+import java.util.List;
+import java.util.Map;
+
+import org.testng.IReporter;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.xml.XmlSuite;
 
-public class ListenersTestNG implements ITestListener {
+public class ListenersTestNG implements ITestListener, IReporter {
 	public void onStart(ITestContext context) {	
 		System.out.println("onStart method started");
 	}
@@ -31,5 +38,24 @@ public class ListenersTestNG implements ITestListener {
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		System.out.println("onTestFailedButWithinSuccessPercentage " + result.getName());
+	}
+
+	@Override
+	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+
+		for (ISuite suite : suites) {
+			String suiteName = suite.getName();
+			Map<String, ISuiteResult> suiteResults = suite.getResults();
+			
+			for (ISuiteResult sr : suiteResults.values()) {
+				ITestContext tc = sr.getTestContext();
+				System.out.println("Passed tests for suite '" + suiteName +
+						"' is:" + tc.getPassedTests().getAllResults().size());
+				System.out.println("Failed tests for suite '" + suiteName +
+						"' is:" + tc.getFailedTests().getAllResults().size());
+				System.out.println("Skipped tests for suite '" + suiteName +
+						"' is:" + tc.getSkippedTests().getAllResults().size());
+			}
+		}
 	}
 }
